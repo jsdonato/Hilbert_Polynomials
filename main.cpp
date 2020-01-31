14c866a63af294806b2ffd5b2d9320951300301f
jsd982412 <jsd982412@gmail.com>
2020-01-31 11:06:35 -0500

    Added functionality for pascals triangle and binomial coefficient

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <vector>
 #include <utility>
+#include <iostream>
 using namespace std;
 
 class Term{
@@ -43,5 +44,39 @@ private:
 };
 
 class Pascal{
+public:
+    Pascal(size_t size){
+        vector<unsigned long long>temp(size, 0);
+        p_triangle.resize(size, temp);
+        
+        p_triangle[0][0] = 1;
+        for (int i = 0; i < p_triangle.size(); i++){
+            p_triangle[i][0] = 1;
+            p_triangle[0][i] = 1;
+        }
+        
+        for (int n = 1; n < p_triangle.size(); n++){
+            for (int m = 1; m < p_triangle.size(); m++){
+                p_triangle[n][m] = p_triangle[n][m - 1] + p_triangle[n - 1][m];
+            }
+        }
+    }
     
+    unsigned long long Binomial(int n, int k){
+        return p_triangle[n - k][k];
+    }
+    
+    
+
+private:
+    vector<vector<unsigned long long>> p_triangle;
 };
+
+
+
+int main(){
+    
+    
+    return 0;
+}
+
